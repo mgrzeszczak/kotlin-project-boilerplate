863c4a4979f1d3fdc4de6c03ca19d29120f4f921
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2018-06-10 16:23:10 +0200

    Update gradle and kotlin version, add two modules

--- app/src/main/kotlin/com/github/mgrzeszczak/kotlinprojectboilerplate/App.kt
+++ app/src/main/kotlin/com/github/mgrzeszczak/kotlinprojectboilerplate/App.kt
@@ -1,14 +1,17 @@
 package com.github.mgrzeszczak.kotlinprojectboilerplate
 
+import com.github.mgrzeszczak.model.ExternalModel
 import mu.KLogging
 
-fun main(args : Array<String>) {
+fun main(args: Array<String>) {
     println("hello world")
     WithLogger().logSomething()
+
+    val externalModel = ExternalModel("name")
 }
 
 class WithLogger {
-    companion object: KLogging()
+    companion object : KLogging()
 
     fun logSomething() {
         logger.info { "hello world" }
---
+++ module/src/main/kotlin/com/github/mgrzeszczak/model/model.kt
@@ -0,0 +1,5 @@
+package com.github.mgrzeszczak.model;
+
+internal data class InternalModel(val name: String)
+
+data class ExternalModel(val name: String)
